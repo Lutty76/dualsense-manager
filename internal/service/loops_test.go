9b70b7bb80f7efa3b3f3ff4e2d9739cab3b24aab
Lutty76 <hidden@address.com>
2026-02-05 10:59:29 +0100

    refacto test

--- internal/service/loops_test.go
+++ internal/service/loops_test.go
@@ -3,35 +3,45 @@ package service
 import "testing"
 
 func TestHexToRGB(t *testing.T) {
-	r, g, b := hexToRGB("FF0000")
-	if r != 255 || g != 0 || b != 0 {
-		t.Fatalf("expected red 255,0,0 got %d,%d,%d", r, g, b)
+
+	tests := []struct {
+		colorHex string
+		want     [3]int
+	}{
+		{"FF0000", [3]int{255, 0, 0}},
+		{"00FF00", [3]int{0, 255, 0}},
+		{"0000FF", [3]int{0, 0, 255}},
+		{"#FF0000", [3]int{255, 0, 0}},
+		{"#00FF00", [3]int{0, 255, 0}},
+		{"#0000FF", [3]int{0, 0, 255}},
+		{"GARBAGE", [3]int{0, 0, 0}},
 	}
 
-	r, g, b = hexToRGB("00FF00")
-	if r != 0 || g != 255 || b != 0 {
-		t.Fatalf("expected green 0,255,0 got %d,%d,%d", r, g, b)
-	}
-
-	r, g, b = hexToRGB("#0000FF")
-	if r != 0 || g != 0 || b != 255 {
-		t.Fatalf("expected blue 0,0,255 got %d,%d,%d", r, g, b)
-	}
-
-	r, g, b = hexToRGB("GARBAGE")
-	if r != 0 || g != 0 || b != 0 {
-		t.Fatalf("expected invalid hex to return 0,0,0 got %d,%d,%d", r, g, b)
+	for _, tt := range tests {
+		r, g, b := hexToRGB(tt.colorHex)
+		if r != tt.want[0] || g != tt.want[1] || b != tt.want[2] {
+			t.Errorf("hexToRGB(%s) = %d,%d,%d; want %d,%d,%d",
+				tt.colorHex, r, g, b, tt.want[0], tt.want[1], tt.want[2])
+		}
 	}
 }
 
 func TestShortMac(t *testing.T) {
-	short := ShortMAC("AA:BB:CC:DD:EE:FF")
-	if short != "EE:FF" {
-		t.Fatalf("expected EE:FF got %s", short)
+
+	tests := []struct {
+		mac  string
+		want string
+	}{
+		{"AA:BB:CC:DD:EE:FF", "EE:FF"},
+		{"11:22:33:44:55:66", "55:66"},
+		{"GARBAGE", "RBAGE"},
 	}
 
-	short = ShortMAC("11:22:33:44:55:66")
-	if short != "55:66" {
-		t.Fatalf("expected 55:66 got %s", short)
+	for _, tt := range tests {
+		short := ShortMAC(tt.mac)
+		if short != tt.want {
+			t.Errorf("ShortMAC(%s) = %s; want %s", tt.mac, short, tt.want)
+		}
 	}
+
 }
